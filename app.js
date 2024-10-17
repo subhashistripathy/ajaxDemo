5de5639909d9a24c33b17bf532d163a1afe6ff30
TanishqPokharia <tppokharia@gmail.com>
2024-10-17 19:24:16 +0530

    third commit

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ const port = 3000
 
 app.use(morgan("dev"))
 app.use(express.static("public"))
-app.use(cors())
+app.use(cors(
+    {
+        origin: '*'
+    }
+))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+})
 app.set("view engine", "ejs")
 
 
