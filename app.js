631ade402b09bf17841e6f5415b09984a29797ca
TanishqPokharia <tppokharia@gmail.com>
2024-10-17 19:21:12 +0530

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
 app.use(morgan("dev"))
 app.use(express.static("public"))
+app.use(cors())
 app.set("view engine", "ejs")
 
 
